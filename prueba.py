8b565f15b476d6ffeff4046e5899ad96d28a4a07
Freddy Cea <freddy.cea@gmail.com>
2013-08-17 08:45:20 -0400

    Ready scraper alimentos, con SQL

--- prueba.py
+++ prueba.py
@@ -11,6 +11,7 @@ import time
 import sys
 import random 
 
+#METODO QUE CREA LA TABLA SQL  EN CASO DE QUE NO EXISTA
 def create_Table_Productos(name):
 	con = mdb.connect('localhost', 'root', 'password', 'LiderWebscraping');
 	with con:
@@ -19,6 +20,7 @@ def create_Table_Productos(name):
 	    #cur.execute("DROP TABLE IF EXISTS "+tableName)
 	    cur.execute("CREATE TABLE IF NOT EXISTS "+name+"(SKU VARCHAR(50),\
 	                Categoria VARCHAR(200) ,\
+	                Cod_Categoria INT ,\
 					Marca VARCHAR(200) ,\
 	                Producto VARCHAR(1000) ,\
   					FechaEjecucion VARCHAR(20) ,\
@@ -28,7 +30,12 @@ def create_Table_Productos(name):
 				  	PrecioNormal INT)")
 	con.close()
 
-def lets_Scrape_Supermarket(url):
+#METODO QUE COORDINA EL SCRAPING DE TODO EL SITIO WEB
+#RECORRE LOS LINKS, DETERMINA LOS QUE SIRVEN. 
+#DE LOS QUE SIRVEN VERIFICA CUALES AUN NO HAN SIDO VISITADOS
+#HACE LA REVISION Y DESPUES LLAMA A LA FUNCION QUE INSERTA EN LA TABLA
+
+def lets_Scrape_Supermarket(url, fecha_ejec, tableName):
 	driver = webdriver.Firefox()
 	driver.get(url)
 	superRef = driver.find_element_by_xpath('/html/body/div[3]/div[3]/ul/li/a')
@@ -36,40 +43,77 @@ def lets_Scrape_Supermarket(url):
 	links = driver.find_elements_by_css_selector(".cursorpointer.alertCheck")
 	linksTmp =[]
 	nombresTmp = []
+	cont=0
 	for ref in links:
 		temp = ref.get_attribute("outerHTML").encode('utf-8').strip()
 		if temp.find("categoryFood") == -1 and temp.find("specialCategoryFood") == -1 and temp.find("CAT_GM") == -1:
 			linksTmp.append("http://www.lider.cl" + get_Link(temp))
-			nombresTmp.append(ref.get_attribute("innerHTML").encode('utf-8').strip())
-	driver.close()
-	print len(linksTmp)
-	print len(nombresTmp)
-	sys.exit(0)
+			nombresTmp.append(str(cont) + "|" + ref.get_attribute("innerHTML").encode('utf-8').strip())
+			cont = cont+1
+	#driver.close()
 	cont = 0
+	#busco id categoria del link mas alta ingresada, asigno el valor al contador e itero desde ese punto
+	cont = data_Inserted(tableName)
 	while cont < len(linksTmp):
-		#reviso si la categoria del link ya fue ingresada para ver desde donde parte el contador
+		print "Ejecuto es " + nombresTmp[cont]
+		resuPart = get_info_Supermarket(driver, linksTmp[cont], fecha_ejec, nombresTmp[cont], tableName) #recupero 7 elementos a insertar, falta la fecha, categoria y cod_cate, pero estan ya aca
+		cont = cont + 1 
+	driver.close()
 
-		driver.get_info(link)
 
+	
+#METODO QUE SIRVE PARA CAMBIAR EL FORMATO DE LOS NUMEROS QUE RECUPERO DESDE LIDER
+#RETORNA LA FECHA SIN SIGNOS $ NI PUNTOS 
+def modify_Prices(formPrice):
+	form1 = formPrice.replace(".","")
+	form2 = form1.replace("$","")
+	return form2
+
+#METODO QUE SIRVE PARA VERIFICAR EL ULTIMO DATO INSERTADO EN LA TABLA SQL 
+#RETORNA 0 SI LA TABLA ESTA VACIA. DE LO CONTRARIO RETORNA EL CODIGO MAS ALTO INGRESADO.
 def data_Inserted(tableName):
 	con = mdb.connect('localhost', 'root', 'password', 'LiderWebscraping');
 	with con:
 	    
 	    cur = con.cursor()
-	    cur.execute("SELECT DISTINCT Categoria FROM " +tableName)
-	    print cur.fetchall()
-
+	    cur.execute("SELECT Cod_Categoria FROM " +tableName)
+	    #Esto me hace pasar la lista a un arreglo
+	    row = [item[0] for item in cur.fetchall()]
 	con.close()
+	try:
+		maxim = max(row)
+		return maxim
+	except ValueError:
+		return 0
 
+#METODO AUXILIAR PARA DEJAR EL LINK CON EL FORMATO APROPIADO
+#RETORNA EL LINK CON TRIMS
 def get_Link(unfText):
 	st = unfText.find('lang')+6
 	nd = unfText.find('"', unfText.find('lang')+6)
 	return unfText[st:nd]
 
+#METODO QUE HACE EL INSERT DENTRO DE LA TABLA SQL 
 
-def get_info(urls, categName, date_ejec): 
+def word_to_SQL_insertion(concatString, tableName):
+
+	con = mdb.connect('localhost', 'root', 'password', 'LiderWebscraping');
+	with con:    
+	    cur = con.cursor()
+	    if cur.execute("SELECT * FROM "+tableName+" WHERE SKU=%s",concatString.split("|")[0]) == 0:
+	    	cur.execute("INSERT INTO  "+tableName+" VALUES (%s,%s,%s,%s, %s,%s,%s,%s,%s, %s)",(concatString.split("|")[0],concatString.split("|")[1],int(concatString.split("|")[2]), concatString.split("|")[3],concatString.split("|")[4],concatString.split("|")[5],int(concatString.split("|")[6]),int(concatString.split("|")[7]),int(concatString.split("|")[8]),int(concatString.split("|")[9])))
+	    else:
+	    	#print 'Nada que hacer con ' + str(concatString.split("|")[0])
+	    	pass
+	con.close()
+
+
+#METODO QUE RECUPERA LA INFORMACION DESDE LA PAGINA (HACE EL WEBSCRAPE )
+#RETORNA 7 PARAMETROS DE INFORMACION AL METODO PRINCIPAL. VAN SEPARADOS POR "|" PARA SPLITEAR DESPUES
+
+def get_info_Supermarket(driver, urls, fecha_ejec, doubleParam, tableName): 
 	#genero la url correcta con todos los elementos cargados
-	driver = webdriver.Firefox()
+	#driver = webdriver.Firefox()
 	driver.get(urls)
 	longit = driver.find_element_by_class_name('pages-shopping').find_element_by_class_name('info')
 	numUnclean = longit.get_attribute("innerHTML").encode('utf-8').strip().split('<strong>')[2]
@@ -129,35 +173,16 @@ def get_info(urls, categName, date_ejec):
 		except: #significa que no hay ningun tag con ese nombre
 			print "Hay un error en el metodo"
 			sys.exit(0)
-		print categName
-		print marcaProd
-		print detalleProd
-		print skuProd
-		print date_ejec
-		print availInd
-		print mainPrice
-		print granelPrice
-		print normalPrice
-	driver.close()
+		#Proceso los parametros que no tengo 	
+		codeCategory = doubleParam.split("|")[0]
+		nameCategory = doubleParam.split("|")[1]
+		#Hago el llamado a la funcion que inserta en la base de datos
+		inputSQL = skuProd +"|"+ nameCategory +"|"+ str(codeCategory) +"|"+ marcaProd +"|"+ detalleProd +"|"+ fecha_ejec +"|"+ str(availInd) +"|"+ str(mainPrice) +"|"+ str(granelPrice) +"|"+ str(normalPrice)
+		word_to_SQL_insertion(inputSQL, tableName)
+	#driver.close()
+	return  
 
-
-def word_to_SQL_insertion(word, fecha_ejec, pagCode):
-
-	con = mdb.connect('localhost', 'root', 'password', 'LiderWebscraping');
-	with con:    
-	    cur = con.cursor()
-	    if cur.execute("SELECT * FROM "+tableAutosName+" WHERE Id=%s",codigo) == 0:
-	    	cur.execute("INSERT INTO  "+tableAutosName+" VALUES (%s,%s,%s,%s, %s,%s,%s,%s,%s)",(codigo,fecha_ejec,pagCode, autName,fotoURL,marcaAuto,modeloAuto,yearAuto,precioAuto))
-	    else:
-	    	#print 'Nada que hacer con ' + str(codigo)
-	    	pass
-	con.close()
-def modify_Prices(formPrice):
-	form1 = formPrice.replace(".","")
-	form2 = form1.replace("$","")
-	return form2
-
-
+#MAIN
 if __name__ == '__main__':
 	#fecha del dia
 	d = datetime.datetime.now().strftime("%Y-%m-%d") 
@@ -165,9 +190,5 @@ if __name__ == '__main__':
 	#inicializo tabla en MySQL
 	LiderSupermarket =  d_name + '_Lider_Supermarket'
 	create_Table_Productos(LiderSupermarket)
-	data_Inserted(LiderSupermarket)	
-
-	#lets_Scrape_Supermarket('http://www.lider.cl/dys/')
-
-	#get_info('http://www.lider.cl/dys/catalog/product/productList.jsp?id=CF_Nivel3_000026&cId=CF_Nivel2_000009&pId=CF_Nivel1_000003&navAction=jump&navCount=2', 'Aceite', '15deAgostoCTM')
-
+	#ejecuto el proceso 
+	lets_Scrape_Supermarket('http://www.lider.cl/dys/',d,LiderSupermarket)
